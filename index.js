bbb32761159a449151c4f2867e2dbad6d22e2397
AJ Gambino <aj.gambino7@gmail.com>
2020-07-21 18:59:14 -0400

    finished

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ console.log-ing the
  following pieces of data 
 
 (a) Home Team name for 2014 world cup final
+
 (b) Away Team name for 2014 world cup final
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
+
+console.log(fifaData[828]["Home Team Name"]);
+console.log(fifaData[828]["Away Team Name"]);
+console.log(fifaData[828]["Home Team Goals"]);
+console.log(fifaData[828]["Away Team Goals"]);
+console.log(fifaData[828]["Home Team Name"]);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and
  returns an array of objects with only finals data */
@@ -73,24 +80,9 @@ function getWinnersByYear(cb, cb2) {
   }
 }
 
-function getWinnersByYear2(cb, cb2) {
-  cb.forEach((item) => {
-    console.log(`In ${item}, ${cb2[7]} won the World Cup!`);
-  });
-}
-
-console.log(
-  getWinnersByYear2(
-    getYears(getFinals(fifaData)),
-    getWinners(getFinals(fifaData))
-  )
-);
-
-console.log(
-  getWinnersByYear(
-    getYears(getFinals(fifaData)),
-    getWinners(getFinals(fifaData))
-  )
+getWinnersByYear(
+  getYears(getFinals(fifaData)),
+  getWinners(getFinals(fifaData))
 );
 
 /* Task 7: Write a function called `getAverageGoals` that accepts a parameter
@@ -108,18 +100,23 @@ console.log(getAverageGoals(fifaData));
 
 /// STRETCH 🥅 //
 
-/* Stretch 1: Create a function called `getCountryWins` that takes the parameters `data` and `team initials` and returns the number of world cup wins that country has had. 
+/* Stretch 1: Create a function called `getCountryWins` that takes the parameters 
+`data` and `team initials` and returns the number of world cup wins that country has had. 
 
 Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
-
-function getCountryWins(/* code here */) {
-  /* code here */
+/*
+function getCountryWins(data, initials) {
+  data.reduce((acc, item) => {
+    return acc + item[initials];
+  });
 }
 
-getCountryWins();
+console.log(getCountryWins(fifaData, "ARG"));
 
-/* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
+/* Stretch 3: Write a function called getGoals() that 
+accepts a parameter `data` and returns the team with the most
+ goals score per appearance (average goals for) in the World Cup finals */
 
 function getGoals(/* code here */) {
   /* code here */
@@ -127,7 +124,9 @@ function getGoals(/* code here */) {
 
 getGoals();
 
-/* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
+/* Stretch 4: Write a function called badDefense() that accepts 
+a parameter `data` and calculates the team with the most goals
+ scored against them per appearance (average goals against) in the World Cup finals */
 
 function badDefense(/* code here */) {
   /* code here */
